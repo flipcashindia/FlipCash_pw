ecf2d67a22c7572e18700f676e15f148c1f381a1
nae-imUam <nae.imuam103@gmail.com>
2026-04-08 17:11:02 +0530

    update wallet

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r25jt8dd62c"
+    "revision": "0.3arj1if0hao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
